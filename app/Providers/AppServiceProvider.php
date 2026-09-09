75cd5d87e2be8fcca34ff3af3be64565af473884
Novandra7 <awangarissaputra@gmail.com>
2026-09-09 12:58:48 +0800

    Refactor: changing to http for testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
         if (app()->environment('production')) {
-            URL::forceScheme('https');
+            URL::forceScheme('http');
         }
     }
 }
